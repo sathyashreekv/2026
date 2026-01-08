1c66c1425f414c4c4fcb032a20b5b05a86353c32
sathyashreekv <sathyashreeshanbogh@gmail.com>
2026-01-08 21:56:00 +0530

    valid palindrome using two pointers

---
+++ valid_palindrome.py
@@ -0,0 +1,17 @@
+def checkPalindrome(s):
+    
+    s=s.lower()
+    for char in s:
+        if not char.isalnum():
+            s=s.replace(char,"")
+    left=0
+    right=len(s)-1
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+
+s="A man, a plan, a canal: Panama"
+print(checkPalindrome(s))
